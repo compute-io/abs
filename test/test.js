ff87d88465e3a8cc2f8527baf400a09a9ea89dff
Planeshifter <pburckhardt@outlook.com>
2015-06-06 21:00:56 +0200

    [UPDATE] tests

--- test/test.js
+++ test/test.js
@@ -5,6 +5,9 @@
 var // Expectation library:
 	chai = require( 'chai' ),
 
+	// Matrix data structure
+	matrix = require( 'dstructs-matrix' ),
+
 	// Module to be tested:
 	abs = require( './../lib' );
 
@@ -23,10 +26,10 @@ describe( 'compute-abs', function tests() {
 		expect( abs ).to.be.a( 'function' );
 	});
 
-	it( 'should throw an error if provided a non-array', function test() {
+	it( 'should throw an error if provided an input which is not array-like or matrix-like', function test() {
 		var values = [
 				5,
-				'5',
+				// '5',
 				{},
 				true,
 				null,
@@ -46,14 +49,102 @@ describe( 'compute-abs', function tests() {
 		}
 	});
 
-	it( 'should compute an element-wise absolute value', function test() {
-		var data, expected;
+	it( 'should throw an error if `options` is not an object', function test() {
+		var values = [
+			'5',
+			5,
+			true,
+			undefined,
+			null,
+			NaN,
+			[],
+			function(){}
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[ i ] ) ).to.throw( TypeError );
+		}
+
+		function badValue( value ) {
+			return function() {
+				abs( [1,2,3,4,5], value );
+			};
+		}
+	});
+
+
+	it( 'should compute an element-wise absolute value for an array', function test() {
+		var data, expected, result;
 
 		data = [ -5, 2, -4, 1, -2, -0 ];
 		expected = [ 5, 2, 4, 1, 2, 0 ];
 
-		abs( data );
-		assert.deepEqual( data, expected );
+		result = abs( data );
+		assert.deepEqual( result, expected );
 	});
+
+	it( 'should compute an element-wise absolute value for an array and mutate it', function test() {
+		var data, expected, result;
+
+		data = [ -5, 2, -4, 1, -2, -0 ];
+		expected = [ 5, 2, 4, 1, 2, 0 ];
+
+		result = abs( data, {
+			'copy': false
+		});
+
+		assert.ok( result === data );
+
+		assert.deepEqual( result, expected );
+	});
+
+	it( 'should compute an element-wise absolute value for an array using an accessor', function test() {
+		var data, expected, result;
+
+		data = [
+			{'x': -5},
+			{'x':  2},
+			{'x': -4},
+			{'x':  1},
+			{'x': -2},
+			{'x': -0} ];
+
+		expected = [ 5, 2, 4, 1, 2, 0 ];
+
+		result = abs( data, {
+			'accessor': getValue
+		});
+		assert.deepEqual( result, expected );
+
+		function getValue( d ) {
+			return d.x;
+		}
+
+	});
+
+	it( 'should compute an element-wise absolute value for a matrix', function test() {
+		var data, expected, results;
+
+		data = matrix( new Int32Array( [ 2, -4, 5, -3, 8, 3, -7, 2, 1 ] ), [3,3] );
+		expected = '2,4,5;3,8,3;7,2,1';
+
+		results = abs( data );
+
+		assert.strictEqual( results.toString(), expected );
+	});
+
+	it( 'should compute an element-wise absolute value for a matrix and mutate the input', function test() {
+		var data, expected, results;
+
+		data = matrix( new Int32Array( [ 2, -4, 5, -3, 8, 3, -7, 2, 1 ] ), [3,3] );
+		expected = '2,4,5;3,8,3;7,2,1';
+
+		results = abs( data, {'copy': false} );
+
+		assert.strictEqual( results.toString(), expected);
+
+		assert.ok( results.data === data.data );
+	});
+
 
 });
