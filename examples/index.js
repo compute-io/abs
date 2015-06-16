3371b89394875a0f2ffa69ed8eb7a27356ed84b9
Planeshifter <pburckhardt@outlook.com>
2015-06-16 23:03:21 +0200

    [UPDATE] examples, Makefile, package.json

--- examples/index.js
+++ examples/index.js
@@ -1,14 +1,84 @@
 'use strict';
 
-var abs = require( './../lib' );
+var matrix = require( 'dstructs-matrix' ),
+	abs = require( './../lib' ),
+	util = require( 'util' );
 
-// Simulate some data...
-var data = new Array( 100 );
+var data,
+	mat,
+	out,
+	tmp,
+	i;
 
-for ( var i = 0; i < data.length; i++ ) {
-	data[ i ] = Math.round( Math.random()*100 ) - 50;
+// ----
+// Plain arrays...
+data = new Array( 10 );
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = Math.random()*20 - 100;
 }
+out = abs( data );
+console.log( 'Arrays: %s\n', out );
 
-abs( data );
 
-console.log( data.join( '\n' ) );
+// ----
+// Object arrays (accessors)...
+function getValue( d ) {
+	return d.x;
+}
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = {
+		'x': data[ i ]
+	};
+}
+out = abs( data, {
+	'accessor': getValue
+});
+console.log( 'Accessors: %s\n', out );
+
+
+// ----
+// Deep set arrays...
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = {
+		'x': [ i, data[ i ].x ]
+	};
+}
+out = abs( data, {
+	'path': 'x/1',
+	'sep': '/'
+});
+console.log( 'Deepset:' );
+console.log( util.inspect( out ) );
+console.log( '\n' );
+
+
+// ----
+// Typed arrays...
+data = new Int32Array( 10 );
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = Math.random() * 20 - 100;
+}
+tmp = abs( data );
+out = '';
+for ( i = 0; i < data.length; i++ ) {
+	out += tmp[ i ];
+	if ( i < data.length-1 ) {
+		out += ',';
+	}
+}
+console.log( 'Typed arrays: %s\n', out );
+
+
+// ----
+// Matrices...
+mat = matrix( data, [5,2], 'int32' );
+out = abs( mat );
+console.log( 'Matrix: %s\n', out.toString() );
+
+
+// ----
+// Matrices (custom output data type)...
+out = abs( mat, {
+	'dtype': 'uint8'
+});
+console.log( 'Matrix (%s): %s\n', out.dtype, out.toString() );
